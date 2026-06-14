04d588045b15be9dafed8e0f2ea8dc291ba761b0
kihit0mi <ondrej.vavera@gmail.com>
2026-06-14 22:42:30 +0200

    Add application entry point

---
+++ jKIS/src/main/java/cz/ikis/ward/Main.java
@@ -0,0 +1,11 @@
+package cz.ikis.ward;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args){
+        SwingUtilities.invokeLater(() -> {
+            System.out.println("iKIS is loading up.");
+        });
+    }
+}
